83c925c2040f7471bd66a3ae97585f6b4c137bf6
SAYEM5522 <md1040582@gmail.com>
2021-12-03 17:03:52 +0600

    Header Add

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from 'react-native';
+import {StatusBar} from "react-native"
+import { EvilIcons } from '@expo/vector-icons';
+const SaerchBarHeight=35;
+const SaerchBarWidth=90;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.Header}>
+        <Text style={styles.HeaderText}>Hello</Text>
+        <View style={styles.SearchBar}>
+        <EvilIcons style={styles.SearchIcon} name="search" size={24} color="black" />
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +21,32 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    top:20,
   },
+  Header:{
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'space-between',
+
+  },
+  HeaderText:{
+    fontSize:30,
+    fontWeight:'bold',
+    marginLeft:20,
+  },
+  SearchIcon:{
+    position:'absolute',
+    left:SaerchBarWidth-40,
+    top:5
+
+  },
+  SearchBar:{
+    width:SaerchBarWidth,
+    height:SaerchBarHeight,
+    borderRadius:SaerchBarHeight/2,
+    borderWidth:1,
+    borderColor:'black',
+    position:'relative',
+    marginRight:50,
+  }
 });
